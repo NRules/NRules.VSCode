34990b6f224653a48d0f732993ded3569ac7a4e9
Sergiy Nikolayev <info@sergiynikolayev.com>
2026-02-08 22:02:48 -0500

    Find NRules session variable in current stack frame

--- src/debuggerProxy.ts
+++ src/debuggerProxy.ts
@@ -2,7 +2,7 @@ import * as vscode from 'vscode';
 
 export class DebuggerProxy {
     public async getContents() {
-        var debugSession = this.getDebugSession();
+        const debugSession = this.getDebugSession();
         if (!debugSession) {
             return "";
         }
@@ -10,16 +10,16 @@ export class DebuggerProxy {
         const threads = await debugSession.customRequest('threads');
         const threadId = threads.threads[0].id;
 
-        const stackTrace = await debugSession.customRequest('stackTrace', 
+        const stackTrace = await debugSession.customRequest('stackTrace',
             {
                 threadId: threadId,
                 startFrame: 0,
                 levels: 1
             });
         const frameId = stackTrace.stackFrames[0].id;
-        let session = "session";
+        const session = await this.findSessionVariable(debugSession, frameId);
 
-        let reply = await debugSession.customRequest('evaluate', 
+        const reply = await debugSession.customRequest('evaluate',
             {
                 expression: `new NRules.Diagnostics.Dgml.DgmlWriter(${session}.GetSchema()).GetContents()`,
                 frameId: frameId,
@@ -33,6 +33,25 @@ export class DebuggerProxy {
         result = result.replace(/\\n/g, '\n');
         
         return result;
+    }
+
+    private async findSessionVariable(debugSession: vscode.DebugSession, frameId: number): Promise<string> {
+        const scopesResponse = await debugSession.customRequest('scopes', { frameId: frameId });
+        const sessionTypePattern = /NRules\.I?Session\b/;
+
+        for (const scope of scopesResponse.scopes) {
+            const variablesResponse = await debugSession.customRequest('variables',
+                { variablesReference: scope.variablesReference });
+
+            for (const variable of variablesResponse.variables) {
+                if (variable.type && sessionTypePattern.test(variable.type)) {
+                    return variable.evaluateName;
+                }
+            }
+        }
+
+        throw new Error("No variable of type NRules.ISession found in the current stack frame. " +
+            "Make sure the debugger is paused in a scope where the NRules session is accessible.");
     }
 
     private getDebugSession(): vscode.DebugSession | undefined {
