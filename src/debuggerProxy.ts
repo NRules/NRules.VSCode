53319a48c29f7b38301c225594ab0926b8b8961d
Sergiy Nikolayev <info@sergiynikolayev.com>
2026-02-08 20:27:25 -0500

    Code cleanup

--- src/debuggerProxy.ts
+++ src/debuggerProxy.ts
@@ -2,26 +2,28 @@ import * as vscode from 'vscode';
 
 export class DebuggerProxy {
     public async getContents() {
-        var session = this.getDebugSession();
-        if (!session) {
+        var debugSession = this.getDebugSession();
+        if (!debugSession) {
             return "";
         }
 
-        const threads = await session.customRequest('threads');
+        const threads = await debugSession.customRequest('threads');
         const threadId = threads.threads[0].id;
 
-        const stackTrace = await session.customRequest('stackTrace', 
+        const stackTrace = await debugSession.customRequest('stackTrace', 
             {
                 threadId: threadId,
                 startFrame: 0,
                 levels: 1
             });
         const frameId = stackTrace.stackFrames[0].id;
+        let session = "session";
 
-        let reply = await session.customRequest('evaluate', 
+        let reply = await debugSession.customRequest('evaluate', 
             {
-                expression: 'new NRules.Diagnostics.Dgml.DgmlWriter(session.GetSchema()).GetContents()',
-                frameId: frameId, context:'repl'
+                expression: `new NRules.Diagnostics.Dgml.DgmlWriter(${session}.GetSchema()).GetContents()`,
+                frameId: frameId,
+                context: 'repl'
             });
 
         let result = reply.result;
